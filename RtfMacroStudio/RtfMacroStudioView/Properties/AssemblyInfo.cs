2fbc6f43d1bd1675c3d758f909ebf5e69ccf982b
relegated <35969849+relegated@users.noreply.github.com>
2020-05-02 09:57:07 -0600

    Initial Push

---
+++ RtfMacroStudio/RtfMacroStudioView/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RtfMacroStudioView")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RtfMacroStudioView")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e6bb8d36-26e6-4d1e-98a8-adb140309681")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RtfMacroStudio/RtfMacroStudioView/StudioViewModelTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using RtfMacroStudioViewModel.ViewModel;
+using System;
+
+
+namespace RtfMacroStudioView
+{
+    [TestFixture]
+    public class StudioViewModelTests
+    {
+        [Test]
+        public void CanCreateViewModel()
+        {
+            var viewModel = new StudioViewModel();
+
+            Assert.That(viewModel != null);
+        }
+    }
+}
